f6c287a26ece2786e211e49e3391f37c7fc88271
Veya Yoan <yove@keystone-sda.ch>
2023-07-10 14:07:38 +0200

    corrections

--- helpers.R
+++ helpers.R
@@ -27,40 +27,12 @@ list_names <- data %>%
 map <- st_read("resources/2023_GEOM_TK/06_POKU/Gesamtfläche_gf/K4_voge20230101_gf/K4voge20230101gf_ch2007Poly.shp") %>%
   clean_names()
 
-
-
-layout <- c(
-  area(t = 1, l = 1, b = 8, r = 8),
-  area(t = 8, l = 8, b = 9, r = 9))
-
 map_name <- function(data, name){
   selected_name <- data %>%
     filter(lastname == name)
   
   join_map <- map %>%
     left_join(selected_name, by = c("id" ="gdenr"))
-  
-  
-  quantile_vec <- join_map %>%
-    pull(pct_gde) %>%
-    quantile(probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
-  
-  join_map <- join_map %>%
-    mutate(quantiles = cut(pct_gde, breaks = quantile_vec, include.lowest = TRUE))
-  
-  labels <- tibble(
-    lab1 = quantile_vec,
-    lab2 = c(quantile_vec[2:length(quantile_vec)], NA)
-  ) %>%
-    slice(1:n() - 1) %>% 
-    mutate(labs = paste(lab1, lab2, sep = " -"))
-  
-  join_map <- join_map %>%
-    mutate(quantiles = cut(pct_gde,
-                           breaks = quantile_vec,
-                           labels = labels$labs,
-                           include.lowest = TRUE
-    ))
   
   join_map %>%
     ggplot() +
